3dfe627ebe60f9b5714c597dfc7aec52cf2fdfb6
Aalish Virani <126311156+Aalish-Virani@users.noreply.github.com>
2024-09-16 10:08:43 +0500

    Revert "update background"
    This reverts commit e15d653ddbb03148e7fa1815f2b697349bfb253d.

--- src/components/BackgroundImage/BackgroundImage.jsx
+++ src/components/BackgroundImage/BackgroundImage.jsx
@@ -1,23 +1,43 @@
 import React, { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 
+import homeDesktop from "../../assets/home/background-home-desktop.jpg";
+import homeTablet from "../../assets/home/background-home-tablet.jpg";
+import homeMobile from "../../assets/home/background-home-mobile.jpg";
+
+import destinationDesktop from "../../assets/destination/background-destination-desktop.jpg";
+import destinationTablet from "../../assets/destination/background-destination-tablet.jpg";
+import destinationMobile from "../../assets/destination/background-destination-mobile.jpg";
+
+import crewDesktop from "../../assets/crew/background-crew-desktop.jpg";
+import crewTablet from "../../assets/crew/background-crew-tablet.jpg";
+import crewMobile from "../../assets/crew/background-crew-mobile.jpg";
+
+import technologyDesktop from "../../assets/technology/background-technology-desktop.jpg";
+import technologyTablet from "../../assets/technology/background-technology-tablet.jpg";
+import technologyMobile from "../../assets/technology/background-technology-mobile.jpg";
+
 const images = {
+  // "home-desktop": homeDesktop,
+  // "home-tablet": homeTablet,
+  // "home-mobile": homeMobile,
 
   "home-desktop": "../src/assets/home/background-home-desktop.jpg",
   "home-tablet": "../src/assets/home/background-home-tablet.jpg",
   "home-mobile": "../src/assets/home/background-home-mobile.jpg",
 
-  "destination-desktop": "../../assets/destination/background-destination-desktop.jpg",
-  "destination-tablet": "../../assets/destination/background-destination-tablet.jpg",
-  "destination-mobile": "../../assets/destination/background-destination-mobile.jpg",
+  "destination-desktop": destinationDesktop,
+  "destination-tablet": destinationTablet,
+  "destination-mobile": destinationMobile,
 
-  "crew-desktop": "../../assets/crew/background-crew-desktop.jpg",
-  "crew-tablet": "../../assets/crew/background-crew-tablet.jpg",
-  "crew-mobile": "../../assets/crew/background-crew-mobile.jpg",
+  "crew-desktop": crewDesktop,
+  "crew-tablet": crewTablet,
+  "crew-mobile": crewMobile,
 
-  "technology-desktop": "../../assets/technology/background-technology-desktop.jpg",
-  "technology-tablet": "../../assets/technology/background-technology-tablet.jpg",
-  "technology-mobile": "../../assets/technology/background-technology-mobile.jpg",
+  "technology-desktop": technologyDesktop,
+  "technology-tablet": technologyTablet,
+  "technology-mobile": technologyMobile,
+
 };
 
 const BackgroundImage = () => {
