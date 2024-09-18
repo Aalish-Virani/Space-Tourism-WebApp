a884bb194e4dbef2918a5496b02e6fec5ed9a1b4
Aalish Virani <126311156+Aalish-Virani@users.noreply.github.com>
2024-09-18 16:18:15 +0500

    Revert "update destination page animation"
    This reverts commit 9cf1e0e6d579ad27d5580b8f314f18adaebc4492.

--- src/Pages/Destination/Destination.jsx
+++ src/Pages/Destination/Destination.jsx
@@ -18,16 +18,15 @@ const Destination = React.memo(({ data }) => {
   ];
 
   return (
-    <section key={id} className=" w-full mx-auto  grid place-items-center px-6 pb-7 gap-5 | sm:px-20 | md:px-[max(36px,4.5vw)] md:pt-5 md:pb-6 md:gap-8 | lg:px-0 lg:pt-0 lg:pb-0 lg:gap-0 lg:grid-rows-12 lg:grid-cols-9 lg:justify-items-start lg:items-center">
+    <section className=" w-full mx-auto  grid place-items-center px-6 pb-7 gap-5 | sm:px-20 | md:px-[max(36px,4.5vw)] md:pt-5 md:pb-6 md:gap-8 | lg:px-0 lg:pt-0 lg:pb-0 lg:gap-0 lg:grid-rows-12 lg:grid-cols-9 lg:justify-items-start lg:items-center">
       <div className="order-1 | md:justify-self-start md:ps-0.5 | lg:row-start-1 lg:row-span-1 lg:col-start-2 lg:col-span-full | xl:row-start-1 xl:row-span-2">
         <PageTitle pageNumber="01" pageHeading="Pick Your Destination" />
       </div>
 
-      {/* planet picture */}
       <motion.picture
-        initial={{ opacity: 0.15 }}
+        initial={{ opacity: 0.25 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: 0.15, duration:0.45 }}
+        transition={{ delay: 0.15 }}
         className="order-2 |  pt-3 pb-0.5 | md:pt-7 md:pb-1.5 | lg:py-0 lg:pt-0 lg:row-start-2 lg:row-end-12 lg:col-start-2 lg:col-span-3 lg:justify-self-end | xl:row-start-3 xl:row-end-12 "
       >
         <img
@@ -36,14 +35,10 @@ const Destination = React.memo(({ data }) => {
           alt="Image"
         />
       </motion.picture>
-
-      {/* navigation bar */}
       <nav className="order-3 | lg:row-start-2 lg:row-span-1 lg:col-start-6 lg:col-span-3  |  xl:row-start-2 xl:row-span-2">
         <UnderlinedIndicators baseRoute="/destination" destinations={planets} />
       </nav>
-
-      <article className=" overflow-hidden | order-4 text-center min-h-[442px] max-w-[492px] | sm:min-h-[465px] sm:max-w-[550px] | md:mx-16 md:min-h-[360px] md:max-w-[600px] | lg:row-start-3 lg:row-end-12 lg:col-start-6 lg:col-span-3 lg:text-start lg:mx-0 lg:max-w-[372px] lg:min-h-[400px] |  xl:max-w-[450px] xl:min-h-[430px] xl:row-start-4 xl:row-end-12">
-        {/* heading */}
+      <article className=" | order-4 text-center min-h-[442px] max-w-[492px] | sm:min-h-[465px] sm:max-w-[550px] | md:mx-16 md:min-h-[360px] md:max-w-[600px] | lg:row-start-3 lg:row-end-12 lg:col-start-6 lg:col-span-3 lg:text-start lg:mx-0 lg:max-w-[372px] lg:min-h-[400px] |  xl:max-w-[450px] xl:min-h-[430px] xl:row-start-4 xl:row-end-12">
         <div className="overflow-hidden">
           <motion.h2
             initial={{ opacity: 0, y: -100 }}
@@ -54,14 +49,12 @@ const Destination = React.memo(({ data }) => {
             {data[id].name}
           </motion.h2>
         </div>
-
-        {/* description */}
+        {/* info */}
         <motion.div
-          initial={{ opacity: 0, x: -50 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.15, duration: 0.15 }}
-          className="overflow-hidden | mx-auto border-b border-primary   pt-0.5 pb-8 | md:pt-2 md:pb-12 | lg:pb-14  "
-        >
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.4, duration:0.05 }}        
+        className="overflow-hidden | mx-auto border-b border-primary   pt-0.5 pb-8 | md:pt-2 md:pb-12 | lg:pb-14  ">
           <motion.p
             initial={{ opacity: 0, y: -100 }}
             animate={{ opacity: 1, y: 0 }}
@@ -71,48 +64,41 @@ const Destination = React.memo(({ data }) => {
             {data[id].description}
           </motion.p>
         </motion.div>
+        <section className=" overflow-hidden | flex flex-col  py-8 gap-8 | md:flex-row md:justify-center md:py-7 md:gap-24 | lg:pb-0 lg:justify-start lg:gap-10 | xl:gap-20">
+          <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.45, duration: 0.15 }}
+            className=""
+          >
+            <h3 className="pb-3 font-primary text-secondary uppercase   text-xs tracking-xl ">
+              avg. distance
+            </h3>
+            <p className="font-secondary uppercase  text-3xl leading-8">
+              {data[id].distance}
+            </p>
+          </motion.div>
 
-        {/* distance and time */}
-        <section className="| flex flex-col  py-8 gap-8 | md:flex-row md:justify-center md:py-7 md:gap-24 | lg:pb-0 lg:justify-start lg:gap-10 | xl:gap-20">
-          <div className=" overflow-hidden">
-            <motion.span
-              initial={{ opacity: 0, y: -100 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.45, duration: 0.15 }}
-              className="inline-block"
-            >
-              <h3 className="pb-3 font-primary text-secondary uppercase   text-xs tracking-xl ">
-                avg. distance
-              </h3>
-              <p className="font-secondary uppercase  text-3xl leading-8">
-                {data[id].distance}
-              </p>
-            </motion.span>
-          </div>
-
-          <div className=" overflow-hidden">
-            <motion.span
-              initial={{ opacity: 0, y: -100 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{
-                delay: window.innerWidth < 768 ? `0.60` : `0.45`,
-                duration: 0.15,
-              }}
-              className="inline-block"
-            >
-              <h3 className="pb-3 font-primary text-secondary  uppercase  text-xs tracking-xl">
-                est. travel time
-              </h3>
-              <p className="font-secondary uppercase  text-3xl leading-8">
-                {data[id].travel}
-              </p>
-            </motion.span>
-          </div>
+          <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: window.innerWidth < 768 ? `0.60` : `0.45`,
+              duration: 0.15,
+            }}
+            className=""
+          >
+            <h3 className="pb-3 font-primary text-secondary  uppercase  text-xs tracking-xl">
+              est. travel time
+            </h3>
+            <p className="font-secondary uppercase  text-3xl leading-8">
+              {data[id].travel}
+            </p>
+          </motion.div>
         </section>
       </article>
     </section>
   );
-
 });
 
 export default Destination;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,12 @@
-import { motion, AnimatePresence, spring, easeInOut } from "framer-motion";
-
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import Logo from "../../assets/shared/logo.svg";
 import NavOpenBtn from "../../assets/shared/icon-hamburger.svg";
 import NavCloseBtn from "../../assets/shared/icon-close.svg";
 
-const Header = React.memo(() => {
+const Header = React.memo( () => {
+
   const pages = [
     { name: "Home", route: "/" },
     { name: "Destination", route: "/destination" },
@@ -15,9 +14,7 @@ const Header = React.memo(() => {
     { name: "Technology", route: "/technology" },
   ];
 
-  const [isMobile, setIsMobile] = useState(
-    window.innerWidth < 768 ? true : false
-  );
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768 ? true : false) 
 
   const [isNavOpen, setIsNavOpen] = useState(false);
 
@@ -25,29 +22,9 @@ const Header = React.memo(() => {
     setIsNavOpen(!isNavOpen);
   };
 
-  const navRef = useRef();
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (navRef.current && !navRef.current.contains(event.target)) {
-        setIsNavOpen(false);
-      }
-    };
-
-    if (isNavOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [isNavOpen]);
-
   return (
-    <header className=" py-6 md:py-0 md:pb-5 lg:py-7 xl:py-10 | font-primary text-primary tracking-3xl flex items-center justify-between">
-      {/* logo */}
+    
+    <header className="py-6 md:py-0 md:pb-5 lg:py-7 xl:py-10 | font-primary text-primary tracking-3xl flex items-center justify-between">
       <NavLink to={"/"} className="px-6 md:px-9 lg:px-14">
         <img className="aspect-square h-10 md:h-12" src={Logo} alt="logo" />
       </NavLink>
@@ -55,37 +32,21 @@ const Header = React.memo(() => {
       {/* header line */}
       <div className="hidden lg:block absolute left-36 right-0 ml-2 mr-[58vw] xl:left-40 xl:ml-10 xl:mr-[57vw] h-px z-20 bg-primary opacity-25 backdrop-blur-lg"></div>
 
-      {/* mobile navigation toggle button */}
-      <motion.button
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.3 }}
-        key={isNavOpen}
-        onClick={toggleNav}
-        className="px-6 z-20 md:hidden"
-      >
+      <button onClick={toggleNav} className="px-6 z-20 md:hidden">
         <img
           src={NavOpenBtn}
-          className={`${
-            isMobile && isNavOpen ? "hidden" : "block"
-          } aspect-square`}
+          className={`${isNavOpen ? "hidden" : "block"} aspect-square`}
         />
         <img
           src={NavCloseBtn}
-          className={`${
-            isMobile && isNavOpen ? "block" : "hidden"
-          } aspect-square`}
+          className={`${isNavOpen ? "block" : "hidden"} aspect-square`}
         />
-      </motion.button>
+      </button>
 
-      {/* navigation for all screens */}
-      <motion.nav
-        initial={(isMobile && !isNavOpen) ? {translateX: "100%"} : ""}
-        animate={(isMobile && isNavOpen) ? {translateX: "32.25%"} : ""}
-        transition={{duration:0.3}}
-        ref={navRef}
-        className={`z-10 fixed inset-0 md:static  
-        bg-white bg-opacity-10 backdrop-blur-lg`}
+      <nav
+        className={`z-10 fixed inset-0 md:static  ${
+          isNavOpen ? "translate-x-1/3" : "translate-x-full md:translate-x-0"
+        } transition-transform duration-300 bg-white bg-opacity-10 backdrop-blur-lg`}
       >
         <ol className=" flex flex-col ps-8 py-36 gap-8 | md:flex-row md:px-12 md:py-10 md:gap-9 md:min-w-[50vw] md:justify-center | lg:px-[max(48px,4.65vw)] lg:gap-10 lg:min-w-[60vw] lg:justify-start | xl:px-[max(123px,8.5vw)] xl:gap-12 2xl:px-[max(128px,10.5vw)] 2xl:gap-14 ">
           {pages.map((page, index) => (
@@ -95,7 +56,7 @@ const Header = React.memo(() => {
             >
               <NavLink
                 to={page.route}
-                onClick={isMobile && isNavOpen ? toggleNav : ""}
+                onClick={isNavOpen ? toggleNav : ""}
                 className={({ isActive }) =>
                   `  | border-primary md:py-[38px] md:border-b  ${
                     isActive
@@ -103,7 +64,6 @@ const Header = React.memo(() => {
                       : "border-opacity-0 hover:border-opacity-25"
                   }  `
                 }
-                
               >
                 <span className="font-bold pr-3 md:hidden lg:inline">{`0${index}`}</span>
                 {page.name}
@@ -111,9 +71,10 @@ const Header = React.memo(() => {
             </li>
           ))}
         </ol>
-      </motion.nav>
+      </nav>
     </header>
   );
-});
+}
+);
 
 export default Header;
