66c5adc2f298298cfc1b6ceac518a4a3e5719933
Aalish Virani <126311156+Aalish-Virani@users.noreply.github.com>
2024-09-19 08:43:37 +0500

    add animation to crew page

--- src/Pages/Crew/Crew.jsx
+++ src/Pages/Crew/Crew.jsx
@@ -1,12 +1,16 @@
+import UseIsMobile from "../../CustomHooks/UseIsMobile.jsx";
+
 import { motion } from "framer-motion";
 
 import React from "react";
 import DottedSlider from "../../components/DottedSlider/DottedSlider";
-import { Outlet, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import PageTitle from "../../components/PageTitle/PageTitle";
 
 const Crew = React.memo(({ data }) => {
   const { id = "0" } = useParams();
+
+  const isMobile = UseIsMobile();
 
   const crew = [
     { name: "Moon", route: "" },
@@ -18,39 +22,61 @@ const Crew = React.memo(({ data }) => {
   const crewImage = import(`${data[id].images.webp}`);
 
   return (
-    <section className="w-full grid  | gap-8 px-6 items-start justify-items-center | sm:px-20 |  md:gap-10 md:px-9 md:items-center | lg:gap-0 lg:px-0 lg:py-0 lg:grid-rows-12 lg:grid-cols-9 lg:items-center lg:justify-items-start | ">
-      {console.log("crew")}
+    <section className=" overflow-hidden | w-full grid  | gap-8 px-6 items-start justify-items-center | sm:px-20 |  md:gap-10 md:px-9 md:items-center | lg:gap-0 lg:px-0 lg:py-0 lg:grid-rows-12 lg:grid-cols-9 lg:items-center lg:justify-items-start | ">
       <div className=" order-1 | md:py-5 md:justify-self-start | lg:order-none lg:row-start-1 lg:row-span-2 lg:col-start-2 lg:col-span-4 | xl:row-start-1 xl:row-span-2 ">
         <PageTitle pageNumber="02" pageHeading="meet your crew" />
       </div>
 
-      <article className=" order-4 min-h-72 max-w-96 text-center | sm:min-h-56 sm:max-w-[550px] | md:order-2 md:px-[119px] md:min-h-[210px] md:max-w-[750px] | lg:px-0 lg:max-w-[450px] lg:min-h-[210px]  lg:order-none lg:row-start-3 lg:row-span-7 lg:col-start-2 lg:col-span-4  lg:text-start | xl:max-w-full xl:min-h-[305px] xl:row-start-3 xl:row-span-7">
-        <h3 className="font-secondary text-primary opacity-50 uppercase  text-base  sm:text-2xl  xl:text-4xl">
+      <article className=" overflow-hidden order-4 min-h-72 max-w-96 text-center | sm:min-h-56 sm:max-w-[550px] | md:order-2 md:px-[119px] md:min-h-[210px] md:max-w-[750px] | lg:px-0 lg:max-w-[450px] lg:min-h-[210px]  lg:order-none lg:row-start-3 lg:row-span-7 lg:col-start-2 lg:col-span-4  lg:text-start | xl:max-w-full xl:min-h-[305px] xl:row-start-3 xl:row-span-7">
+        <motion.h3
+          animate={{ opacity: [0.25, 0.4, 0.55, 0.7, 0.85, 1] }}
+          transition={{ delay: 0.15, duration: 0.75 }}
+          key={id}
+          className="font-secondary text-primary opacity-50 uppercase  text-base  sm:text-2xl  xl:text-4xl"
+        >
           {data[id].role}
-        </h3>
+        </motion.h3>
         <div className="pt-2 pb-4 | xl:pt-4 xl:pb-7">
-          <h2 className="font-secondary text-primary uppercase  text-2xl  sm:text-5xl  xl:text-6xl">
+          <motion.h2
+            animate={{ opacity: [0.15, 0.3, 0.45, 0.6, 0.75, 1] }}
+            transition={{ delay: 0.2, duration: 0.7 }}
+            key={id}
+            className="font-secondary text-primary uppercase  text-2xl  sm:text-5xl  xl:text-6xl"
+          >
             {data[id].name}
-          </h2>
+          </motion.h2>
         </div>
-        <p className="font-tertiary text-secondary text-sm   sm:text-base sm:leading-7  xl:text-lg xl:max-w-[45ch] ">
+        <motion.p
+          animate={{ opacity: [0.05, 0.2, 0.35, 0.5, 0.65, 1] }}
+          transition={{ delay: 0.25, duration: 0.65 }}
+          key={id + 1}
+          className="font-tertiary text-secondary text-sm   sm:text-base sm:leading-7  xl:text-lg xl:max-w-[45ch] "
+        >
           {data[id].bio}
-        </p>
+        </motion.p>
       </article>
 
       <nav className="order-3 | lg:order-none lg:row-end-13 lg:row-span-3 lg:col-start-2 lg:col-span-4 | xl:row-end-13 xl:row-span-3">
         <DottedSlider destinations={crew} baseRoute={"/crew"} />
       </nav>
 
-      <motion.picture className=" overflow-hidden order-2 w-full py-[1px] border-b border-secondary border-opacity-25 | md:order-4 md:self-end md:border-none | lg:order-none  lg:row-start-1 lg:row-span-full lg:col-end-10 lg:col-span-4 ">
+      <motion.picture className=" overflow-hidden order-2 w-full py-[1px]  | md:order-4 md:self-end | lg:order-none  lg:row-start-1 lg:row-span-full lg:col-end-10 lg:col-span-4 ">
         <motion.img
-          initial={{ y: 100, opacity: 1 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 3 }}
+          initial={isMobile ? { y: "75%" } : { y: "25%" }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.9 }}
+          key={id}
           src={`${data[id].images.webp}`}
           className="mx-auto min-h-56 h-[30vh] | md:min-h-[532px] md:h-[50vh] | lg:mx-0 lg:me-auto lg:min-h-[450px] lg:h-[75vh] lg:max-h-[615px] | xl:min-h-[550px] xl:max-h-[760px] "
           alt=""
         />
+        <motion.div
+          initial={{ scaleX: 0.25 }}
+          animate={{ scaleX: 1 }}
+          transition={{ delay: 0.15, duration: 0.75 }}
+          key={id + 1}
+          className="w-full h-[1px] bg-secondary opacity-25 md:opacity-0"
+        ></motion.div>
       </motion.picture>
     </section>
   );
